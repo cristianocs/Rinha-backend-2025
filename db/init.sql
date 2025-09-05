0eb28d30af9b4f6a42f73c6f5600f79843c1cb3f
Cristiano Costa <cristiano.costa@hardware.org.br>
2025-09-05 14:06:47 -0300

    removing comments

--- db/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE payments (
-    id SERIAL PRIMARY KEY,
-    correlation_id UUID NOT NULL UNIQUE,
-    amount DECIMAL(10, 2) NOT NULL,
-    processor VARCHAR(10) NOT NULL,
-    requested_at TIMESTAMPTZ NOT NULL
-);
-
-CREATE INDEX idx_payments_requested_at ON payments (requested_at);
